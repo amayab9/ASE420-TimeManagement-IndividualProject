8b33f30427d7d4d0c9542479c685e86939cd18e1
Amaya Bryant <100623332+amayab9@users.noreply.github.com>
2023-10-24 19:04:56 -0400

    program can now take todays date via today or specified daye yyyy/mm/dd, take a task as a string (more than one word), recognize query command, query based off today, query based off all, query based off a tag, and query based off task

--- program.py
+++ program.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 import sqlite3
-from datetime import date
 
 conn = sqlite3.connect("timeManagement.db")
 cursor = conn.cursor()
@@ -14,57 +13,85 @@ cursor.execute('''
     task TEXT,
     tag TEXT
     )
-
 ''')
 conn.commit()
 
+while True:
+    print("Enter record DATE FROM TO TASK TAG\nOR\nquery query_search (q to quit)")
+    user_input = input()
 
-print("Enter record DATE FROM TO TASK TAG")
-user_input = input()
+    if user_input.lower() == 'q':
+        break
 
-input_values = user_input.split()
+    input_values = user_input.split()
 
-today = date.today()
+    today = None
 
-if len(input_values) == 2:
-    command, query_request = input_values
-
-
-if len(input_values) == 6:
-    command, user_input_date, user_input_from_time, user_input_to_time, user_input_task, user_input_tag = input_values
-    try:
-        user_input_date = datetime.strptime(user_input_date, "%Y/%m/%d").date()
-
+    if len(input_values) >= 5:
+        command = input_values[0]
         if command.lower() == "record":
+            user_input_date = input_values[1]
+            user_input_from_time = input_values[2]
+            user_input_to_time = input_values[3]
+
+            task_tag_string = ' '.join(input_values[4:])
+
+            task_parts = task_tag_string.split(':')
+
+            if len(task_parts) > 1:
+                user_input_task = task_parts[0].strip()
+                user_input_tag = task_parts[1].strip()
+            else:
+                user_input_task = task_parts[0].strip()
+                user_input_tag = ""
 
             if user_input_date.lower() == "today":
-                user_input_date = today
+                today = datetime.now().strftime("%Y/%m/%d")
+            else:
+                try:
+                    datetime.strptime(user_input_date, "%Y/%m/%d")
+                    today = user_input_date
+                except ValueError:
+                    print("Invalid date format")
+                    continue
 
+            if len(input_values) >= 5:
                 cursor.execute("INSERT INTO time_records (date, from_time, to_time, task, tag) VALUES (?, ?, ?, ?, ?)",
-                               (user_input_date, user_input_from_time, user_input_to_time, user_input_task, user_input_tag))
+                               (today, user_input_from_time, user_input_to_time, user_input_task, user_input_tag))
                 conn.commit()
                 print("Record submitted successfully")
             else:
-                print("invalid")
+                print("Invalid input format. You can provide a tag after the task.")
         else:
             print("Invalid command. Try again")
-    except ValueError:
-        print("invalid date format")
 
-else:
-    print("Invalid input format.")
+    elif len(input_values) == 2:
+        command, query_request = input_values
 
-cursor.execute("SELECT * FROM time_records")
-records = cursor.fetchall()
+        if command.lower() == "query":
+            if query_request.lower() == "all":
+                cursor.execute("SELECT * FROM time_records")
+            elif query_request.lower() == "today":
+                today = datetime.now().strftime("%Y/%m/%d")
+                cursor.execute("SELECT * FROM time_records WHERE date = ?", (today,))
+            elif query_request.startswith(":"):
+                tag = query_request[1:]
+                cursor.execute("SELECT * FROM time_records WHERE LOWER(tag) = LOWER(?)", (tag,))
+            else:
+                cursor.execute("SELECT * FROM time_records WHERE task = ?", (query_request,))
 
+            results = cursor.fetchall()
 
-if records:
-    for record in records:
-        id, date, from_time, to_time, task, tag = record
-        task = task.strip("'")
-        print(f"ID: {id}, Date: {date}, From: {from_time}, To: {to_time}, Task: {task}, Tag: {tag}")
-else:
-    print("No records found.")
+            if results:
+                for record in results:
+                    id, date, from_time, to_time, task, tag = record
+                    print(f"ID: {id}, Date: {date}, From: {from_time}, To: {to_time}, Task: {task}, Tag: {tag}")
+            else:
+                print("No records found.")
+        else:
+            print("Command not recognized")
 
+    else:
+        print("Invalid input format.")
 
 conn.close()
